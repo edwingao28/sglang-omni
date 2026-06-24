811c2feb0966ac4baa2d4cf827ea9a42207f5f54
Wenyao Gao <wgao11@u.rochester.edu>
2026-06-23 21:49:59 -0700

    [feat] restore standalone semantic reference path

--- tests/unit_test/ming_omni/test_diffusion_imports.py
+++ tests/unit_test/ming_omni/test_diffusion_imports.py
@@ -18,8 +18,11 @@ def test_ming_diffusion_backend_contract_imports() -> None:
 def test_ming_diffusion_lightweight_modules_import() -> None:
     modules = [
         "sglang_omni.models.ming_omni.diffusion",
+        "sglang_omni.models.ming_omni.diffusion.bailing_moe_config",
+        "sglang_omni.models.ming_omni.diffusion.bailing_moe_model",
         "sglang_omni.models.ming_omni.diffusion.query_info",
         "sglang_omni.models.ming_omni.diffusion.semantic_conditioner",
+        "sglang_omni.models.ming_omni.diffusion.semantic_encoder",
         "sglang_omni.models.ming_omni.diffusion.zimage_backend",
     ]
 
